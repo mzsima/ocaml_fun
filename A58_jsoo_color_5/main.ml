09f8def8922e5ebdb76ecd6c5f3b4f3603e13874
yusuke mizushima <mzsima@gmail.com>
2022-08-31 00:13:31 +0900

    color 5

---
+++ A58_jsoo_color_5/main.ml
@@ -0,0 +1,38 @@
+open Js_of_ocaml
+open Graphics_js
+
+module Html = Dom_html
+let pi = acos (-1.)
+
+type point = {x : float; y : float}
+type canvasElement = Html.canvasElement Js.t  
+type canvasCtx = Html.canvasRenderingContext2D Js.t
+
+class colorbar =
+  object
+
+    method draw (ctx : canvasCtx)  = 
+      ctx##.lineWidth := 0.;
+      ctx##translate 40. 40. ;
+      let r = 24.0 in
+      for i = 0 to 19 do
+        let fillColor = Color.of_hsl ((float i) *. 18.) 1.0 0.5 in
+        ctx##.fillStyle := Js.string (Color.to_css_hsla fillColor) ;
+        for j = 0 to 10 do
+          let x, y = r *. (float i), 2. *. r *. ((float j) +. if i mod 2 == 0 then 0. else 0.5) in
+          ctx##beginPath ;
+          ctx##arc x y r 0. (pi *. 2.0) (Js.bool false) ;
+          ctx##fill ;
+        done
+      done
+end
+
+let init canvas =
+  print_endline "initializing";
+  let () = open_canvas canvas in
+  let c = canvas##getContext Html._2d_ in
+  let colorbar = new colorbar  in
+  colorbar#draw c ;
+  ()
+
+let () = Js.export "init" init
